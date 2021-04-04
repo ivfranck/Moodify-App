a112d8854cf4de4ee311be7943afddcdc128fef6
Wandja Ivan <ivanwandja99@gmail.com>
2021-04-04 18:58:23 +0200

    added header to diary

---
+++ diary.php
@@ -0,0 +1,11 @@
+<?php include_once "header_footer/header.php";?>
+<div class="diary-ent">
+    <h1 class="title">My Personal Diary</h1>
+        <form id="entryForm" class="align-middle">
+            <textarea class="entry-textbox" placeholder="What happened today?" required></textarea><br/>
+            <button type="submit" class="button">Submit</button>
+        </form>
+</div>
+    <script src="js/diary.js"></script>
+  </body>
+</html>
--- header_footer/header.php
+++ header_footer/header.php
@@ -13,7 +13,7 @@ session_start();
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <!--<link rel="stylesheet" href="../authentication/css/bootstrap.css">-->
-
+    <link href="css/diary.css" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../authentication/css/form.css">
@@ -42,17 +42,17 @@ session_start();
         </li>
         <?php
         if(isset($_SESSION["userName"])){
+            echo "<li><a href='#'>About</a></li>";
+            echo "<li><a href='../diary.php'>Diary</a></li>";
             echo "<li><a href='../authentication/profile.php'>Profile</a></li>";
             echo "<li><a href='../authentication/includes/logout-inc.php'>Logout</a></li>";
         }
         else{
             echo "<li><a href='../authentication/signup.php'>Create account</a></li>";
             echo "<li><a href='../authentication/login.php'>Login</a></li>";
+            echo "<li><a href='#'>About</a></li>";
         }
         ?>
-        <li>
-            <a href="">About</a>
-        </li>
 
     </ul>
 
